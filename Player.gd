352936f0eaf9fd639437d69da797f95b1cd311b3
Benedikt Ziemons <benedikt.ziemons@athox.de>
2019-08-07 00:47:20 +0200

    Start with adding player and a base scene

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+# Declare member variables here.
+var speed = 200
+var screen_size
+var player_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	player_size = get_node("Sprite").texture.get_size()
+	position = (screen_size / 2) - (player_size / 2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x - player_size.x)
+	position.y = clamp(position.y, 0, screen_size.y - player_size.y)
