32b42d3ebc374184d41369906c6efeaad4a3776a
Milchm4nn <23378574+Milchm4nn@users.noreply.github.com>
2019-08-08 22:13:35 +0200

    Add spiral algorithm for map generator

--- WorldMap.gd
+++ WorldMap.gd
@@ -38,3 +38,42 @@ func _ready():
 				set_cellv(tile + Vector2(0, -1), 0)
 		elif x == 5:
 			set_cellv(tile + Vector2(0, -2), 0)
+	
+	clear()
+	tile = Vector2(7, 7)
+	set_cellv(tile, 0)
+	for a in range(1000):
+		for b in range(a):
+			if tile.y as int % 2 == 0:
+				set_cellv(tile + Vector2(0, -1), 1)
+				tile += Vector2(0, -1)
+			else:
+				set_cellv(tile + Vector2(1, -1), 1)
+				tile += Vector2(1, -1)
+		for b in range(a - 1):
+			if tile.y as int % 2 == 0:
+				set_cellv(tile + Vector2(0, 1), 2)
+				tile += Vector2(0, 1)
+			else:
+				set_cellv(tile + Vector2(1, 1), 2)
+				tile += Vector2(1, 1)
+		for b in range(a):
+				set_cellv(tile + Vector2(0, 2), 3)
+				tile += Vector2(0, 2)
+		for b in range(a):
+			if tile.y as int % 2 == 0:
+				set_cellv(tile + Vector2(-1, 1), 4)
+				tile += Vector2(-1, 1)
+			else:
+				set_cellv(tile + Vector2(0, 1), 4)
+				tile += Vector2(0, 1)
+		for b in range(a):
+			if tile.y as int % 2 == 0:
+				set_cellv(tile + Vector2(-1, -1), 5)
+				tile += Vector2(-1, -1)
+			else:
+				set_cellv(tile + Vector2(0, -1), 5)
+				tile += Vector2(0, -1)
+		for b in range(a):
+			set_cellv(tile + Vector2(0, -2), 6)
+			tile += Vector2(0, -2)
