f8a457e1c5f8cd0706c7b7ebd07f867ee7b843b6
Milchm4nn <23378574+Milchm4nn@users.noreply.github.com>
2019-08-13 23:56:28 +0200

    Add tile types and first draft of popup

--- WorldMap.gd
+++ WorldMap.gd
@@ -1,6 +1,18 @@
 extends TileMap
 
 onready var camera = get_node("Camera2D")
+#Walking speed is calculated by
+#(normalSpeed * ((tiredness / 100) + (illness / 100) + 1) * ((density / 10) + 1 
+#Event is calculated by
+var tile_type = [
+	{ "density": 3, "zombie": 6, "raider": 7, "survivor": 2, "name": "road" },
+	{ "density": 9, "zombie": 1, "raider": 3, "survivor": 0, "name": "water" },
+	{ "density": 5, "zombie": 8, "raider": 5, "survivor": 3, "name": "building" },
+	{ "density": 1, "zombie": 3, "raider": 4, "survivor": 2, "name": "grass" },
+	{ "density": 7, "zombie": 2, "raider": 7, "survivor": 4, "name": "mountain" },
+	{ "density": 4, "zombie": 6, "raider": 10, "survivor": 1, "name": "military" },
+	{ "density": 6, "zombie": 4, "raider": 8, "survivor": 4, "name": "forest" } 
+]
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -19,6 +31,15 @@ func _input(event):
 				terrainInfoLabel.text = tile_set.tile_get_name(get_cellv(map_position))
 				# Todo: Map panel to a fixed location on the camera
 				terrainInfoPanel.popup(Rect2(Vector2(100, 100), Vector2(200, 100)))
+		if event.button_index == BUTTON_RIGHT:
+			if event.pressed == false:
+				terrainInfoPanel.hide()
+				var map_position = world_to_map(event.position + camera.position - get_viewport().size / 2)
+				if get_cellv(map_position) != INVALID_CELL:
+					camera.add_child(terrainInfoPanel)
+					terrainInfoPanel.add_child(terrainInfoLabel)
+					terrainInfoLabel.text = get_info(tile_set.tile_get_name(get_cellv(map_position)))
+					terrainInfoPanel.popup(Rect2(map_to_world(map_position) + cell_size, Vector2(100, 50)))
 	elif event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(BUTTON_LEFT):
 			camera.position -= event.relative
@@ -92,3 +113,16 @@ func _ready():
 		for b in range(a):
 			set_cellv(tile + Vector2(0, -2), 6)
 			tile += Vector2(0, -2)
+
+func get_info(type_id):
+	var info_string = """{name}
+	
+	Density: {density}
+	Zombie: {zombie}
+	Raider: {raider}
+	Survivor: {survivor}"""
+	return info_string.format(tile_type[int(type_id)])
+
+func get_density(type_id):
+	return tile_type[int(type_id)].density
+	
